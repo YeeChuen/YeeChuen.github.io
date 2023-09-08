ed4975bb529f4cdee2b769c0a53d42608c924aa1
YeeChuen <tychuen98@gmail.com>
2023-09-08 17:39:11 -0500

    Fix bug where route doesn't work

--- src/App.js
+++ src/App.js
@@ -14,45 +14,22 @@ import { Text, Flex, Box } from 'rebass';
 import Typography from './components/Typography';
 import AppBar from './components/Appbar';
 
-import { Link, Router, Route, Routes, useLocation, BrowserRouter } from 'react-router-dom';
+import { Link, Router, Route, Routes, useLocation, BrowserRouter, HashRouter } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <BrowserRouter>
         <AppBar/>
-        {/* <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
-        <div className='Title'>
-          <Flex
-            px={2}
-            color='black'
-            alignItems='center'
-          >
-            
-            <a className={'TitleLink'} href='/'>
-              <Typography
-                component="h1"
-                variant="h5"
-                color="black"
-                className="imageTitle"
-                fontWeight="bold"
-              >
-                Title
-              </Typography>
-            </a>
-          </Flex>
-        </div> */}
         <Routes>
-          <Route path = '/' element={<HomePage />} />
-          <Route path = '/resume' element={<Resume />} />
-          <Route path = '/portfolio' element={<Portfolio />} />
-          <Route path = '/portfolio/software' element={<Software />} />
-          <Route path = '/portfolio/architecture' element={<Architecture />} />
-          <Route path = '/about' element={<AboutMe />} />
-          <Route path = '/contact' element={<Contact />} />
+          <Route path = '' element={<HomePage />} />
+          <Route path = 'resume' element={<Resume />} />
+          <Route path = 'portfolio' element={<Portfolio />} />
+          <Route path = 'portfolio/software' element={<Software />} />
+          <Route path = 'portfolio/architecture' element={<Architecture />} />
+          <Route path = 'about' element={<AboutMe />} />
+          <Route path = 'contact' element={<Contact />} />
         </Routes>
         <Footer />
-      </BrowserRouter>
     </>
   );
 }
